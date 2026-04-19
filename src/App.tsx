a49a1507a00d09cb54815524b67e255191f962b7
Rajath <raorajath1998@gmail.com>
2026-04-19 19:23:36 +0530

    image-rendering-fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ function App() {
     <div className="w-dvw min-h-dvh bg-custom-slate-300 grid place-items-center font-outfit">
       <div className="w-[320px] h-124.75 bg-white rounded-[20px] p-4 flex flex-col gap-6 items-center text-center">
         <div className="size-72 rounded-[10px] overflow-clip">
-          <img src="/image-qr-code.png" alt="qr-code" className="size-full" />
+          <img
+            src={import.meta.env.BASE_URL + "/image-qr-code.png"}
+            alt="qr-code"
+            className="size-full"
+          />
         </div>
         <div className="flex flex-col gap-4 ">
           <div className="text-custom-slate-900 text-[22px] font-bold">
